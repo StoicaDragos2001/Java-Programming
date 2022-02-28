880648d71e3e413ee9c7ad87459932f6dbe28f9d
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-02-28 16:16:19 +0200

    Delete finaldisplay.java

--- Laborator 1/Compulsory/finaldisplay.java
+++
@@ -1,8 +0,0 @@
-package Java;
-
-public class finaldisplay {
-    public void learn(int n){
-        array object=new array();
-        System.out.println("Willy-nilly, this semester I will learn " + object.define(n));
-    }
-}
