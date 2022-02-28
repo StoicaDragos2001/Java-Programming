752d41fc602f1b44fb4228c8a53fdd5491ce2964
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-02-28 16:15:58 +0200

    Delete array.java

--- Laborator 1/Compulsory/array.java
+++
@@ -1,8 +0,0 @@
-package Java;
-
-public class array {
-    public String define(int n) {
-        String languages[] = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
-        return languages[n];
-    }
-}
