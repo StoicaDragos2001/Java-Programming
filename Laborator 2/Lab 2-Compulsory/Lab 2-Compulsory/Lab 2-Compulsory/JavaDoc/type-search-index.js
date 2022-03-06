9b50a2f653c9823507ba2ac9b535b01a7b8734fe
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-03-06 21:58:25 +0200

    Delete Laborator 2/Lab 2-Compulsory/Lab 2-Compulsory directory

--- Laborator 2/Lab 2-Compulsory/Lab 2-Compulsory/Lab 2-Compulsory/JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Event"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Room"},{"p":"<Unnamed>","l":"RoomType"}];updateSearchResults();
