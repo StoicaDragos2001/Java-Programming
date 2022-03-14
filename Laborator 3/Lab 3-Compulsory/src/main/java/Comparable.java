be871d20025308158c31f6a67556025772b37af2
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-03-14 12:01:37 +0200

    Add files via upload

---
+++ Laborator 3/Lab 3-Compulsory/src/main/java/Comparable.java
@@ -0,0 +1,7 @@
+/**
+ *
+ * @param <Node> este de tip nod si folosim metoda compareTo pt natural ordering
+ */
+public interface Comparable<Node> extends java.lang.Comparable<Node> {
+    int compareTo(Node object);
+}
