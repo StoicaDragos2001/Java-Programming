9a2c3993cfdb8a72ee28515917ed58cd0e9eb7e0
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-03-14 11:58:26 +0200

    Add files via upload

---
+++ Laborator 3/Lab 3-Compulsory/Lab 3-Bonus/src/main/java/Router.java
@@ -0,0 +1,40 @@
+/**
+ * Clasa Router
+ * @author Petrea Daniela
+ */
+
+/**
+ * nodul de tip Router poate fi identificat prin adresa
+ */
+public class Router extends Node implements Identifiable{
+    private String address;
+
+    Router(String name, String address) {
+        super(name);
+        setAddress(address);
+    }
+
+    /**
+     * metoda de set
+     * @param address adresa nodului de tip Router
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * suprascriem metoda getAddress din interfata Identifiable
+     * @return adresa
+     */
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "Router{" +
+                "name=" + getName() + " address="+ getAddress() + " }";
+    }
+
+}
