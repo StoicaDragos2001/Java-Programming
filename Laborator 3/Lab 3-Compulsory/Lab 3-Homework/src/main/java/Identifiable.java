141da6a2381b0f4b7096ae2b01d1519d7b1df7e8
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-03-14 11:56:20 +0200

    Add files via upload

---
+++ Laborator 3/Lab 3-Compulsory/Lab 3-Homework/src/main/java/Identifiable.java
@@ -0,0 +1,11 @@
+/**
+ * @author Petrea Daniela
+ * clasa Identifiable
+ */
+
+/**
+ * in interfata Identifiable declar metoda getAdress pe care o suprascriu in clasa Computer si Router
+ */
+public interface Identifiable {
+    String getAddress();
+}
