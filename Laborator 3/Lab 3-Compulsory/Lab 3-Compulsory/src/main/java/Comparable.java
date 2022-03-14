8682b5e8c21278467eb5109df8761383c20fb6fe
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-03-14 12:00:48 +0200

    Delete Laborator 3/Lab 3-Compulsory directory

--- Laborator 3/Lab 3-Compulsory/Lab 3-Compulsory/src/main/java/Comparable.java
+++
@@ -1,7 +0,0 @@
-/**
- *
- * @param <Node> este de tip nod si folosim metoda compareTo pt natural ordering
- */
-public interface Comparable<Node> extends java.lang.Comparable<Node> {
-    int compareTo(Node object);
-}
