113ffe88b2f6ddc8f8caa37febe4a49d2320c620
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-03-19 09:13:15 +0200

    Add files via upload

---
+++ Laborator 4/Lab 4-Compulsory/Lab4-Compulsory/src/main/java/Instances/Comparable.java
@@ -0,0 +1,8 @@
+package Instances;
+/**
+ * @author Petrea Daniela
+ * @param <Street>
+ */
+public interface Comparable<Street> extends java.lang.Comparable<Street> {
+    int compareTo(Street object);
+}
