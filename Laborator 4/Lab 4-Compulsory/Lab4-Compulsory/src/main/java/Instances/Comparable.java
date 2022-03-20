5182783aa585dc41e02f2aaecf1060ddd65fc9f2
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-03-20 20:27:43 +0200

    Delete Laborator 4/Lab 4-Compulsory/Lab4-Compulsory directory

--- Laborator 4/Lab 4-Compulsory/Lab4-Compulsory/src/main/java/Instances/Comparable.java
+++
@@ -1,8 +0,0 @@
-package Instances;
-/**
- * @author Petrea Daniela
- * @param <Street>
- */
-public interface Comparable<Street> extends java.lang.Comparable<Street> {
-    int compareTo(Street object);
-}
