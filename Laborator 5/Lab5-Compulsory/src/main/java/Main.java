6423d2ec0aed478337554eb7b7272cef2f08a849
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-03-21 18:44:40 +0200

    Update Main.java

--- Laborator 5/Lab5-Compulsory/src/main/java/Main.java
+++ Laborator 5/Lab5-Compulsory/src/main/java/Main.java
@@ -1,3 +1,13 @@
-public class Main {
+import catalog.Catalog;
+import catalog.CatalogUtil;
+import instances.Article;
+import instances.Book;
 
+public class Main {
+    Catalog c = new Catalog("C:\\Users\\Daniela-211021\\IdeaProjects\\Lab5-Compulsory\\catalog.txt");
+    Book item1 = new Book("knuth67", "Pride and prejudice", "C:\\Users\\Daniela-211021\\IdeaProjects\\Lab5-Compulsory\\pride_and_prejudice.pdf", 1813, "Jane Austen", "book");
+    Article item2 = new Article("java17", "The Java Language Specification", "https://docs.oracle.com/javase/specs/jls/se17/html/index.html", 2021, "James Gosling & others", "article");
+    CatalogUtil catalog=new CatalogUtil();
+    //catalog.add(c,item1);
+    //catalog.add(c,item2);
 }
