052d5279f74b66cf56b7523b11ffe96ba4d55645
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-04-04 17:00:49 +0300

    Add files via upload

---
+++ Laborator 6/Lab6/target/classes/panel/ConfigPanel.java
@@ -0,0 +1,56 @@
+package panel;
+
+/**
+ * @author Petrea Daniela & Stoica Dragos-Stefan
+ * ConfigPanel class
+ */
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.util.LinkedList;
+
+public class ConfigPanel extends JPanel {
+    final MainFrame frame;
+    JLabel label;
+    JSpinner spinner1;
+    JSpinner spinner2;
+    JButton create;
+
+    public ConfigPanel(MainFrame frame) {
+        this.frame = frame;
+        init();
+    }
+
+    /**
+     * to init the ConfigPanel
+     */
+    private void init() {
+        label = new JLabel("Grid size:");
+        spinner1 = new JSpinner(new SpinnerNumberModel(10, 2, 100, 1));
+        spinner1.setValue(10);
+        spinner2 = new JSpinner(new SpinnerNumberModel(10, 2, 100, 1));
+        spinner2.setValue(10);
+        create = new JButton("Create");
+        add(label); //JPanel uses FlowLayout by default
+        add(spinner1);
+        add(spinner2);
+        add(create);
+        create.addActionListener(this::create);
+    }
+
+
+    public int getRows() {
+        return (int) spinner1.getValue();
+    }
+
+    public int getCols() {
+        return (int) spinner2.getValue();
+    }
+
+    private void create(ActionEvent e) {
+        this.frame.canvas.init(getRows(), getCols());
+        this.frame.canvas.repaint();
+        this.frame.canvas.setCircles(new LinkedList<>());
+        this.frame.canvas.ok=0;
+    }
+}
---
+++ Laborator 6/Lab6/target/classes/panel/ControlPanel.java
@@ -0,0 +1,76 @@
+package panel;
+
+/**
+ * @author Petrea Daniela & Stoica Dragos-Stefan
+ * ControlPanel class
+ */
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class ControlPanel extends JPanel {
+    final MainFrame frame;
+    JButton saveBtn = new JButton("Save");
+    JButton loadBtn = new JButton("Load");
+    JButton exitBtn = new JButton("Exit");
+
+    public ControlPanel(MainFrame frame) {
+        this.frame = frame;
+        init();
+    }
+
+    /**
+     * to init the ControlPanel
+     */
+    private void init() {
+        //change the default layout manager (just for fun)
+        setLayout(new GridLayout(1, 3));
+
+        // Add all the buttons
+        add(saveBtn, BorderLayout.CENTER);
+        add(loadBtn, BorderLayout.CENTER);
+        add(exitBtn, BorderLayout.CENTER);
+
+        //configure listeners for all buttons
+        saveBtn.addActionListener(this::save);
+        loadBtn.addActionListener(this::load);
+        exitBtn.addActionListener(this::exit);
+
+    }
+
+    private void save(ActionEvent event) {
+        frame.pack();
+        BufferedImage img = new BufferedImage(frame.canvas.canvasWidth, frame.canvas.canvasHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = img.createGraphics();
+        frame.canvas.paint(g2d);
+        try {
+            ImageIO.write(img, "png", new File("C:\\Users\\Daniela-211021\\IdeaProjects\\Lab6\\frame.png"));
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    private void load(ActionEvent event) {
+        this.frame.canvas.setCols(10);
+        this.frame.canvas.setRows(10);
+        this.frame.canvas.init(10, 10);
+        this.frame.configPanel.spinner1.setValue(10);
+        this.frame.configPanel.spinner2.setValue(10);
+        this.frame.canvas.repaint();
+        this.frame.canvas.setCircles(new LinkedList<>());
+        this.frame.canvas.ok = 0;
+    }
+
+    private void exit(ActionEvent event) {
+        System.out.println("Exit with success!");
+        frame.dispose();
+    }
+
+
+}
