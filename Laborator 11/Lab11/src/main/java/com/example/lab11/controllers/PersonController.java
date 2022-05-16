2e2571ddb15ab2abdf43cc0745417a2e0461a26d
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-05-16 11:43:11 +0300

    Add files via upload

--- Laborator 11/Lab11/src/main/java/com/example/lab11/controllers/PersonController.java
+++ Laborator 11/Lab11/src/main/java/com/example/lab11/controllers/PersonController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * class PersonController
+ * @authors: Petrea Daniela & Stoica Dragos
+ */
 @RestController
 public class PersonController {
     private final PersonService personService;
@@ -16,16 +20,30 @@ public class PersonController {
         this.personService = personService;
     }
 
+    /**
+     * creating a get mapping for the testing
+     */
+
     @GetMapping("/test")
     public String test() {
         return "Hello";
     }
+
+    /**
+     * creating a get mapping that retrieves all the persons detail from the database
+     * @return a list with persons
+     */
 
     @GetMapping("person/list")
     public List<Person> getUsers() {
         return personService.getUsers();
     }
 
+    /**
+     * creating post mapping that post the person detail in the database
+     * @param name - name of the person
+     * @return a message
+     */
     @PostMapping("person/add")
     public String addUser(@RequestParam String name) {
         System.out.println(name);
@@ -39,16 +57,33 @@ public class PersonController {
         return "Person added successfully";
     }
 
+    /**
+     * creating put mapping that updates the person name
+     * @param name - actual name
+     * @param name1 - updated name
+     * @return a message
+     */
     @PutMapping("person/update")
-    public String updateUser(@RequestParam String name) {
+    public String updateUser(@RequestParam String name,@RequestParam String name1) {
         Person person = personService.getPersonByName(name);
         if (person != null) {
-            person.setName(name);
-            personService.updatePerson(person);
-            return "Person updated successfully";
+            personService.deletePerson(person);
+            Person person1 = personService.getPersonByName(name);
+            if (person1 == null) {
+                person = new Person();
+                person.setName(name1);
+                personService.updatePerson(person);
+                return "Person updated successfully";
+            } else
+                return "Person already exists";
         }
         return "Person does not exist";
     }
+
+    /**
+     * @param name - name of the person
+     * @return a message
+     */
 
     @DeleteMapping("person/delete")
     public String deleteUser(@RequestParam String name) {
--- Laborator 11/Lab11/src/main/java/com/example/lab11/services/PersonService.java
+++ Laborator 11/Lab11/src/main/java/com/example/lab11/services/PersonService.java
@@ -1,13 +1,14 @@
 package com.example.lab11.services;
-
 import com.example.lab11.entity.Person;
 import com.example.lab11.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * class PersonService
+ * authors: Petrea Daniela & Stoica Dragos
+ */
 @Service
 public class PersonService {
     private final PersonRepository personRepository;
@@ -16,20 +17,46 @@ public class PersonService {
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
+
+    /**
+     * find a person by name implementing the method findPersonByName
+     * @param name - name of the person
+     * @return the person with the given name, else null
+     */
     public Person getPersonByName(String name) {
         return personRepository.findPersonByName(name);
     }
-    public void addPerson(Person person) {
-        personRepository.save(person);
-    }
+
+    /**
+     * getting all users record by using the method findaAll() of CrudRepository
+     */
 
     public List<Person> getUsers() {
         return personRepository.findAll();
     }
 
+    /**
+     * saving a specific record by using the method save() of CrudRepository
+     * @param person
+     */
+
+    public void addPerson(Person person) {
+        personRepository.save(person);
+    }
+
+    /**
+     * updating a record
+     * @param person
+     */
+
     public void updatePerson(Person person) {
         personRepository.save(person);
     }
+
+    /**
+     * deleting a specific record by using the method delete() of CrudRepository
+     * @param person
+     */
 
     public void deletePerson(Person person) {
         personRepository.delete(person);
