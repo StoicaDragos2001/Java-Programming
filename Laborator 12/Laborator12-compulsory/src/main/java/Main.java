cef7edf481a85e30181699ed0ac80da7999c0e47
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-05-30 16:15:26 +0300

    Update Main.java

--- Laborator 12/Laborator12-compulsory/src/main/java/Main.java
+++ Laborator 12/Laborator12-compulsory/src/main/java/Main.java
@@ -10,49 +10,37 @@ import java.lang.reflect.*;
 public class Main {
     public static void main(String[] args) {
         String absolutePath = "C:\\Users\\Daniela-211021\\IdeaProjects\\Laborator12-compulsory\\target\\classes\\newClass\\MyTestClass";
-        boolean found = false;
         String pathToClasses = absolutePath.substring(0, absolutePath.lastIndexOf("\\"));
         String packageAndClass = absolutePath.substring(absolutePath.lastIndexOf("\\") + 1);
         try {
+            packageAndClass = pathToClasses.substring(pathToClasses.lastIndexOf("\\") + 1) + "." + packageAndClass;
+            pathToClasses = pathToClasses.substring(0, pathToClasses.lastIndexOf("\\"));
             File path = new File(pathToClasses);
             if (path.exists()) {
                 Class cls = Class.forName(packageAndClass);
-            }
-        } catch (ClassNotFoundException e) {
-            while (found == false) {
-                try {
-                    packageAndClass = pathToClasses.substring(pathToClasses.lastIndexOf("\\") + 1) + "." + packageAndClass;
-                    pathToClasses = pathToClasses.substring(0, pathToClasses.lastIndexOf("\\"));
-                    File path = new File(pathToClasses);
-                    if (path.exists()) {
-                        System.out.println(packageAndClass);
-                        Class cls = Class.forName(packageAndClass);
-                        System.out.println("Classname: " + cls.getName());
-                        System.out.println("Path: " + pathToClasses);
-                        System.out.println("PackageAndClass: " + packageAndClass);
-                        System.out.println("Methods:");
-                        for (Method method : Class.forName(packageAndClass).getMethods()) {
-                            System.out.printf("%s %s(", method.getReturnType(), method.getName());
-                            for (var parameter : method.getParameterTypes()) {
-                                System.out.printf(" %s ", parameter.getCanonicalName());
-                            }
-                            System.out.printf(")\n");
-                        }
-                        System.out.println("Test methods:");
-                        for (Method method : Class.forName(packageAndClass).getMethods()) {
-                            if (method.isAnnotationPresent(Test.class)) {
-                                try {
-                                    method.invoke(null);
-                                } catch (Throwable exception) {
-                                    System.out.println(exception.getMessage());
-                                }
-                            }
-                        }
-                        break;
+                System.out.println("Classname: " + cls.getName().substring(cls.getName().lastIndexOf(".")+1));
+                System.out.println("Path: " + pathToClasses);
+                System.out.println("PackageAndClass: " + packageAndClass);
+                System.out.println("Methods:");
+                for (Method method : Class.forName(packageAndClass).getMethods()) {
+                    System.out.printf("%s %s(", method.getReturnType(), method.getName());
+                    for (var parameter : method.getParameterTypes()) {
+                        System.out.printf(" %s ", parameter.getCanonicalName());
                     }
-                } catch (ClassNotFoundException ex) {
+                    System.out.printf(")\n");
+                }
+                System.out.println("Test methods:");
+                for (Method method : Class.forName(packageAndClass).getMethods()) {
+                    if (method.isAnnotationPresent(Test.class)) {
+                        try {
+                            method.invoke(null);
+                        } catch (Throwable exception) {
+                            System.out.println(exception.getMessage());
+                        }
+                    }
                 }
             }
+        } catch (ClassNotFoundException ex) {
         }
     }
 }
